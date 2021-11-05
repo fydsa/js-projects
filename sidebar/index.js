dfb17eb3237815200debc4418e44241fb16bdedb
Saba <saba.alikhani@ymail.com>
2021-11-05 23:35:15 +0330

    Added the initial version of the icons

--- sidebar/index.js
+++ sidebar/index.js
@@ -5,15 +5,26 @@ const indicator = document.querySelector(".indicator");
 
 const nav = document.querySelector("nav");
 
+const svg_path = document.querySelectorAll("path");
+
+svg_path.forEach((el) => el.setAttribute("fill", "#c0c0c0"));
+
+svg_path.forEach((el) => el.setAttribute("stroke-width", "1")); //#c0c0c0
+
+svg_path.forEach((el) => el.setAttribute("stroke", "#c0c0c0"));
+
 buttons.forEach((button) => button.addEventListener("click", clickItem));
 
 function clickItem(e) {
   let selected_item = e.target;
+
   let inner_text = selected_item.querySelector("span");
   inner_text.className = "selected-text";
 
   indicator.style.top = `${
-    -nav.getBoundingClientRect().top + selected_item.getBoundingClientRect().top
+    -nav.getBoundingClientRect().top +
+    selected_item.getBoundingClientRect().top -
+    2
   }px`;
 
   buttons_text.forEach((el) => {
