979782230634981838b74db4af824226bb7ab26c
Saba <saba.alikhani@ymail.com>
2021-11-05 19:44:28 +0330

    Applied the changes to make the indicator move to the selected item

--- sidebar/index.js
+++ sidebar/index.js
@@ -3,12 +3,18 @@ const buttons_text = document.querySelectorAll("button > div > span");
 
 const indicator = document.querySelector(".indicator");
 
+const nav = document.querySelector("nav");
+
 buttons.forEach((button) => button.addEventListener("click", clickItem));
 
 function clickItem(e) {
   let selected_item = e.target;
   let inner_text = selected_item.querySelector("span");
   inner_text.className = "selected-text";
+
+  indicator.style.top = `${
+    -nav.getBoundingClientRect().top + selected_item.getBoundingClientRect().top
+  }px`;
 
   buttons_text.forEach((el) => {
     if (el !== inner_text) {
