2a5ebede35f43f3460e11528a8d6dd7b2fc30926
fydsa <saba.alikhani@ymail.com>
2022-01-06 14:41:29 +0330

    Added the navigating route for the home page

--- food-order/src/App.js
+++ food-order/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Navbar from "./components/navbar/navbar.component";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
-  return <div className="App">hey</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* <Route />
+          <Route />
+          <Route />
+          <Route /> */}
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- food-order/src/components/navbar/navbar.component.js
+++ food-order/src/components/navbar/navbar.component.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 import "./navbar.style.css";
+import { Hamburger } from "../../icons/hamburger.icon";
 
 function Navbar() {
+  const [showLinks, setShowLinks] = useState(true);
+
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <div className="navbar">
-      <div className="nav-left">
+      <div className="nav-left" id={showLinks ? "close" : "open"}>
         <img src={logo} />
+        <div className="hide-links">
+          <Link to="home">Home</Link>
+          <Link to="menu">Menu</Link>
+          <Link to="about">About</Link>
+          <Link to="contact">Contact</Link>
+        </div>
       </div>
       <div className="nav-right">
         <Link to="home">Home</Link>
         <Link to="menu">Menu</Link>
         <Link to="about">About</Link>
         <Link to="contact">Contact</Link>
+        <button onClick={handleShowLinks}>
+          <Hamburger
+            className="h-12 w-12"
+            fill="red"
+            strokeWidth={3}
+            stroke={"white"}
+          />
+        </button>
       </div>
     </div>
   );
