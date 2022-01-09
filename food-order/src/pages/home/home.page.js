e64d20b70fbda5b0e031e029348b79fb889eb9d0
fydsa <saba.alikhani@ymail.com>
2022-01-09 19:50:04 +0330

    Defined the home details and styles

--- food-order/src/pages/home/home.page.js
+++ food-order/src/pages/home/home.page.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./home.style.css";
 
-const Home = () => {
-  return <></>;
+const Menu = () => {
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>Food Corner</h1>
+        <p>INDIAN FOOD AT A CLICK</p>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default Menu;
