05142cb2d4f192201c5bf0eb45ca348720420722
fydsa <saba.alikhani@ymail.com>
2022-01-10 14:56:30 +0330

    Added the contact page

--- food-order/src/pages/contact/contact.page.js
+++ food-order/src/pages/contact/contact.page.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./contact.style.css";
 
 const Contact = () => {
-  return <></>;
+  return (
+    <div class="contact">
+      <div class="left-side"></div>
+      <div class="right-side">
+        <h1>Contact Us</h1>
+        <form id="contact-form" method="POST">
+          <label htmlFor="name">Full Name</label>
+          <input name="name" placeholder="Enter full name..." type="text" />
+          <label htmlFor="email">Email</label>
+          <input name="email" placeholder="Enter email..." type="email" />
+          <label htmlFor="message">Message</label>
+          <textarea
+            rows="6"
+            placeholder="Enter message..."
+            name="message"
+            required
+          ></textarea>
+          <button type="submit"> Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
